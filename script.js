ababff1c52e7d041c518ba631d839783e4a06b75
kazi251 <kai.chan.tsuru@gmail.com>
2025-05-10 13:08:18 +0900

    a

--- script.js
+++ script.js
@@ -149,9 +149,7 @@ async function loadPalette(callback, silent = false) {
   } catch (error) {
     console.error("読み込みに失敗しました:", error);
   }
-} catch (error) {
-    console.error("読み込みに失敗しました:", error);
-  }
+}
 
 async function saveStatus() {
   const hp = document.getElementById("hp-input").value;
