47378c39e84f928ed781b270ad502101cd5e06c1
kazi251 <kai.chan.tsuru@gmail.com>
2025-05-11 23:00:28 +0900

    a

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const playerId = urlParams.get("playerId") || "default";
 let currentCharacterId = null;
 
 function updateDisplay() {
+    console.log("updateDisplay が呼ばれました");
     const sanMax = document.getElementById("san-max-input").value;
     document.getElementById("san-indef").textContent = Math.floor(sanMax * 0.8);
 
