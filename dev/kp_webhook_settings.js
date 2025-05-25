ef83df77d39522248519d8091f3c0d929f13622b
kazi251 <kai.chan.tsuru@gmail.com>
2025-05-25 23:41:55 +0900

    a

--- dev/kp_webhook_settings.js
+++ dev/kp_webhook_settings.js
@@ -48,7 +48,7 @@ async function fetchThreads() {
 function renderThreadList() {
   threadList.innerHTML = "";
   threadListCache.forEach(thread => {
-    const li = document.createElement("li");
+    
     const nameSpan = document.createElement("span");
     nameSpan.className = "thread-name";
     li.textContent = thread.name;
