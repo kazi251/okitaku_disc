a8eba86b27432b654e52c2d88bd59bd9f4d84418
kazi251 <kai.chan.tsuru@gmail.com>
2025-05-29 14:43:29 +0900

    a

--- dev/player.js
+++ dev/player.js
@@ -407,12 +407,13 @@ async function saveCharacterData() {
         `\`\`\``;
 
       const avatarUrl = imageSrc;
-      const statusThreadId = currentCharacterData?.webhook?.statusWebhook || null;
+      const statusThreadId = currentCharacterData?.statusWebhook || null;
+      console.log("statusThreadId1:", statusThreadId);
       let webhook = null;
 
       // statusWebhookが存在すれば対応するURLを取得
       if (statusThreadId && currentCharacterData?.scenarioId) {
-          console.log("statusThreadId:", statusThreadId);
+          console.log("statusThreadId2:", statusThreadId);
           console.log("scenarioId:", currentCharacterData.scenarioId);
         try {
           const threadRef = doc(db, "scenarios", currentCharacterData.scenarioId, "threads", statusThreadId);
