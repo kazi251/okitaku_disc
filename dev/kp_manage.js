44211683896eb204886b62a8d69abc53de146088
kazi251 <kai.chan.tsuru@gmail.com>
2025-05-31 15:33:37 +0900

    a

--- dev/kp_manage.js
+++ dev/kp_manage.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
-  getFirestore, collection, getDocs,doc
+  getFirestore, collection, getDocs, doc, setDoc
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { showToast } from './utils.js';
 
